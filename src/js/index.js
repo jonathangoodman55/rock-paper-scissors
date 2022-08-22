57bb429c0497fac4b2402afe91c3966955c6fadb
Jonathan Goodman <jonathangoodman@hotmail.com>
2022-08-22 13:04:19 +0100

    Show buttons for each player action

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,22 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-import logger from './logger';
+import { actionMap  } from "./actions";
 
 import '../css/index.scss';
 
-logger('it works well!');
+const actions = Object.keys(actionMap);
+
+const playerActionItems = actions.map((key) => {
+  const li = document.createElement("li");
+  const buttonElement = document.createElement("button");
+  buttonElement.id = key;
+  buttonElement.title = actionMap[key].helpText;
+  buttonElement.innerHTML = actionMap[key].display;
+  buttonElement.addEventListener("click", () => console.log('do something'))
+  li.append(buttonElement);
+  return li;
+});
+
+const playerActionList = document.getElementById("player-actions");
+playerActionList.append(...playerActionItems);
