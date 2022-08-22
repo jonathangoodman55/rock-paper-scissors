d6e3e1a1bb2847230cd532984f3b3e7ba0879010
Jonathan Goodman <jonathangoodman@hotmail.com>
2022-08-22 14:23:26 +0100

    Adds test for existing restart behaviour

--- cypress/e2e/rock-paper-scissors.cy.js
+++ cypress/e2e/rock-paper-scissors.cy.js
@@ -100,3 +100,57 @@ describe('When user chooses to play aginst the computer', () => {
   })
 });
 
+describe('When user plays a game and wants to restart', () => {
+  it('Should take the user to a new instance of the game ', () => {
+    cy.visit('http://localhost:8080');
+
+    cy
+      .get('ul#player-actions')
+      .get('button')
+      .contains('Rock')
+      .click();
+
+    cy
+      .get('button')
+      .contains('Restart')
+      .click();
+    
+    cy.get('h1').should(
+      'contain',
+      'Rock Paper Scissors'
+    )
+
+    cy.get('section#play').get('h2').should('contain', "Player actions");
+
+    cy
+      .get('ul#player-actions')
+      .get('button')
+      .contains('Rock')
+      .should('exist');
+
+    cy
+      .get('ul#player-actions')
+      .get('button')
+      .contains('Paper')
+      .should('exist');
+
+    cy
+      .get('ul#player-actions')
+      .get('button')
+      .contains('Scissors')
+      .should('exist');
+
+    cy.get('section#play').get('h2').should('contain', "Other actions");
+    
+    cy
+      .get('ul#player-actions')
+      .get('button')
+      .contains('Computer vs Computer')
+      .should('exist');
+
+    cy
+      .get('#result')
+      .should('not.exist')
+  })
+});
+
