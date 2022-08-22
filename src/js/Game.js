833aa56b248a314850385cf268a6ee21410dc3a8
Jonathan Goodman <jonathangoodman@hotmail.com>
2022-08-22 11:16:07 +0100

    Build play and winner determining logic

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,3 +1,41 @@
 export class Game {
-    
+  constructor(spec) {
+    this.spec = spec;
+    this.player1Name = 'Player 1';
+    this.player2Name = 'Player 2';
+  }
+
+  determineWinner() {
+    if (!this.player1Selection || !this.player2Selection) {
+      throw new Error("Player selection required first");
+    }
+
+    if (this.spec[this.player1Selection].beats === this.player2Selection) {
+      return 1;
+    }
+
+    if (this.spec[this.player2Selection].beats === this.player1Selection) {
+      return 2;
+    }
+
+    return 0;
+  }
+
+  play(player1Selection, player2Selection) {
+    this.player1Selection = player1Selection;
+    this.player2Selection = player2Selection
+    this.side = this.determineWinner();
+  }
+
+  result() {
+    if (this.side === 1) {
+      return `${this.player1Name} won 🎉`;
+    }
+
+    if (this.side === 2) {
+      return `${this.player2Name} won 🎉`;
+    }
+
+    return "It was a draw 🤷🏼‍♂️";
+  }
 }
--- test/game.spec.js
+++ test/game.spec.js
@@ -1,25 +1,45 @@
 import { Game } from '../src/js/Game';
 
 describe('Game', () => {
-  describe('When player 1 uses as action that beats player 2', () => {
-    it('Should say player 1 wins', () => {
-      const actionSpec = {
-        rock: {
-          beats: "scissors"
-        },
-        paper: {
-          beats: "rock"
-        },
-        scissors: {
-          beats: "paper"
-        }
-      };
 
-      const game = new Game(actionSpec);
-      game.play('rock', 'scissors');
+  describe('With tradional actions', () => {
+    const actionSpec = {
+      rock: {
+        beats: "scissors"
+      },
+      paper: {
+        beats: "rock"
+      },
+      scissors: {
+        beats: "paper"
+      }
+    };
 
+    describe('When player 1 chooses action that beats player 2', () => {
+      it('Should say player 1 wins', () => {
+        const game = new Game(actionSpec);
+        game.play('rock', 'scissors');
 
-      expect(game.result).toBe('Player 1 wins');
-    })
+        expect(game.result()).toBe('Player 1 won 🎉');
+      });
+    });
+
+    describe('When player 2 chooses action that beats player 1', () => {
+      it('Should say player 1 wins', () => {
+        const game = new Game(actionSpec);
+        game.play('scissors', 'rock');
+
+        expect(game.result()).toBe('Player 2 won 🎉');
+      });
+    });
+
+    describe('When both players choose the same action', () => {
+      it('Should say that they drew', () => {
+        const game = new Game(actionSpec);
+        game.play('rock', 'rock');
+
+        expect(game.result()).toBe('It was a draw 🤷🏼‍♂️');
+      });
+    });
   });
 });
