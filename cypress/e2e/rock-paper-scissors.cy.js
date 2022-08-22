1781ac071d1d11faa2edeb4d33867e2fe70826bb
Jonathan Goodman <jonathangoodman@hotmail.com>
2022-08-22 12:57:07 +0100

    Begin first UI e2e test

--- cypress/e2e/rock-paper-scissors.cy.js
+++ cypress/e2e/rock-paper-scissors.cy.js
@@ -1,5 +1,16 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:8080')
+describe('First Glance', () => {
+  it('Displays a page with traditional game actions', () => {
+    cy.visit('http://localhost:8080');
+    cy.get('h1').should(
+      'contain',
+      'Rock Paper Scissors'
+    )
+    cy.get('section#play').get('h2').should('contain', "Player actions");
+    cy
+      .get('ul#player-actions')
+      .get('button')
+      .contains('Rock')
+      .should('exist');
+     
   })
 })
